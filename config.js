8727af62b6b08ca0742f36206ffeb179539e1b12
Gary Borton <gary.borton@airbnb.com>
2016-01-01 14:32:25 -0800

    Initial commit.

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    "nickname": "slack-irc-bridge",
+    "server": "irc.freenode.com",
+    "channelMapping": {
+      "#general": "#screeps"
+    },
+    "token": process.env.TOKEN
+  }
+]
